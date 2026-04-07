074fdcc7647ab207e06bb057c9dc5c88337dbad6
omedeiroz <contato.medeirosart@gmail.com>
2026-04-06 22:15:24 -0300

    ajustar o endereço de escuta do servidor para '0.0.0.0'

--- server.js
+++ server.js
@@ -20,6 +20,6 @@ app.get('/health', (req, res) => {
   res.json({ status: 'OK' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
